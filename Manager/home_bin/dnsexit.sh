bbd2f96bf521069e99048d349f06dcaa14760c7b
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2017-09-01 11:07:33 -0500

    Create dnsexit.sh

---
+++ Manager/home_bin/dnsexit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#pull the params from the config file
+. ~/etc/dnsexit.conf
+
+echo DNSIP = $DNSIP
+echo IP = $IP
+
+if [ "$DNSIP" != "$IP" ]; then
+  echo "updating $HOST from $DNSIP to $IP
+  curl -s "http://update.dnsexit.com/RemoteUpdate.sv?login=$USERID&password=$PASSWORD&host=$HOST&myip=$IP"
+fi
