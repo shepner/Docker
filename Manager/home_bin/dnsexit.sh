a1f8278282da36fb210cc1a3234b3ab0665c4e26
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2017-09-20 16:06:00 -0500

    Update dnsexit.sh

--- Manager/home_bin/dnsexit.sh
+++ Manager/home_bin/dnsexit.sh
@@ -6,6 +6,16 @@
 #echo DNSIP = $DNSIP
 #echo IP = $IP
 
+echo "\n\nverify the URL\n"
+curl s 'http://www.dnsexit.com/ipupdate/dyndata.txt'
+
+echo "\n\nvalidate the credentials\n"
+curl -s 'http://update.dnsexit.com/ipupdate/account_validate.jsp?login=$USERID&password=$PASSWORD'
+
+echo "\n\nverify the domain(s)\n"
+curl -s 'http://update.dnsexit.com/ipupdate/domains.jsp?login=$USERID'
+
+echo "\n\nupdate the IP\n"
 if [ "$DNSIP" != "$IP" ]; then
   echo "updating $HOST from $DNSIP to $IP"
   curl -s 'http://update.dnsexit.com/RemoteUpdate.sv?login=$USERID&password=$PASSWORD&host=$HOST&myip=$IP'
