4972142575f01e709e9b20c0f19775fa4dd8ff46
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-30 18:55:01 -0600

    Create d_create.sh

---
+++ Manager/HOME/bin/d_create.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#####################################
+# DONT GO BLINDLY RUNNING THIS!
+#####################################
+
+#this page has some good stuff for rewriting this into something better:
+#https://codefresh.io/blog/deploy-docker-compose-v3-swarm-mode-cluster/
+
+
+#FIX DNS FIRST OR USE IP ADDRESSES INSTEAD!
+
+nodes[0]="de01a"
+nodes[1]="de01b"
+nodes[2]="de02a"
+nodes[3]="de02b"
+nodes[4]="de03a"
+nodes[5]="de03b"
+
+for DE in ${nodes[@]} ; do
+  #setup the ssh keys
+  ssh-copy-id -i ~/.ssh/dockerengine_rsa dockerengine@$DE
+  
+  #create the docker engines
+  docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=$DE $DE
+done
