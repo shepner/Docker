6e3e18657dcf807aee999461b9de48e8eaade6ce
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-30 20:44:46 -0600

    Update and rename d_create.sh to dc_install_nodes.sh

--- Manager/HOME/bin/dc_install_nodes.sh
+++ Manager/HOME/bin/dc_install_nodes.sh
@@ -3,14 +3,15 @@
 # DONT GO BLINDLY RUNNING THIS!
 #####################################
 
-#FIX DNS FIRST OR USE IP ADDRESSES INSTEAD!
-
 nodes[0]="de01a"
 nodes[1]="de01b"
 nodes[2]="de02a"
 nodes[3]="de02b"
 nodes[4]="de03a"
 nodes[5]="de03b"
+
+#show what is available
+docker-machine ls
 
 for DE in ${nodes[@]} ; do
   #setup the ssh keys
