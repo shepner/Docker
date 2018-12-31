ac70e0e6d29be58a23ee965e350ea19a69701f79
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-30 18:29:46 -0600

    Update dm-create.sh

--- Manager/home_bin/dm-create.sh
+++ Manager/home_bin/dm-create.sh
@@ -1,38 +1,39 @@
 #!/bin/sh
 
-# FIX DNS FIRST OR USE IP ADDRESSES INSTEAD!
-
-# UPDATE THIS SECTION!  NOT USING RANGER
+# FIX DNS SO IP IS NOT NEEDED
 #create the docker engines
-#docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=rancher --generic-ip-address=de01a de01a
-#docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=rancher --generic-ip-address=de01b de02a
-#docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=rancher --generic-ip-address=de01b de03a
+docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=10.0.0.71 de01a
+docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=10.0.0.72 de02a
+docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=10.0.0.73 de03a
+docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=10.0.0.74 de01b
 
 # show what is available
 sudo docker-machine ls
 
 # make this machine a swarm manager
-sudo docker swarm init
+sudo docker swarm init --advertise-addr 10.1.3.18
 
 # to add another manager
 #sudo docker swarm join-token manager
 
 # to re-obtain the swarm join token
-#sudo docker swarm join-token worker
+TOKEN-`sudo docker swarm join-token -q worker`
 
 # add workers
 # NOTE:  the `docker swarm init` string is what you will be using but make sure the IP address is one the workers can talk to
-docker-machine ssh de01a "docker swarm join --token SWMTKN-1-4[...]1h83 10.1.3.18:2377"
-docker-machine ssh de02a "docker swarm join --token SWMTKN-1-4[...]1h83 10.1.3.18:2377"
-docker-machine ssh de03a "docker swarm join --token SWMTKN-1-4[...]1h83 10.1.3.18:2377"
-
-# do this to fix a broken node
-#docker-machine ssh de03a "docker swarm leave"
-#docker-machine ssh de03a "docker swarm join --token SWMTKN-1-4[...]1h83 10.1.3.18:2377"
-
+docker-machine ssh de01a "docker swarm join --token $TOKEN 10.1.3.18:2377"
+docker-machine ssh de02a "docker swarm join --token $TOKEN 10.1.3.18:2377"
+docker-machine ssh de03a "docker swarm join --token $TOKEN 10.1.3.18:2377"
 
 #show the cluster
 sudo docker node ls
+
+# do this to fix a broken node
+NODE=de03a
+TOKEN=`sudo docker swarm join-token -q worker`
+docker-machine ssh $NODE "docker swarm leave"
+docker-machine ssh $NODE "docker swarm join --token $TOKEN 10.1.3.18:2377"
+
 
 # make sure ~/docker-compose.yml exists
 
