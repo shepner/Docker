533871fda93ba99340563cd7fb9f3499530182ec
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-31 15:28:23 -0600

    Update dc_install_nodes.sh

--- Manager/HOME/bin/dc_install_nodes.sh
+++ Manager/HOME/bin/dc_install_nodes.sh
@@ -3,24 +3,26 @@
 # DONT GO BLINDLY RUNNING THIS!
 #####################################
 
-nodes[0]="de01a"
-nodes[1]="de01b"
-nodes[2]="de02a"
-nodes[3]="de02b"
-nodes[4]="de03a"
-nodes[5]="de03b"
+nodes[0]="de01"
+nodes[1]="de02"
+nodes[2]="de03"
+nodes[3]="de04"
+nodes[4]="de05"
+nodes[5]="de06"
 
 # show what is available
 docker-machine ls
 
 # setup the ssh keys
 for DE in ${nodes[@]} ; do
-  ssh-copy-id -i ~/.ssh/dockerengine_rsa dockerengine@$DE
+  ssh-copy-id -i ~/.ssh/docker_rsa docker@$DE
 done
 
 # create the docker engines
 for DE in ${nodes[@]} ; do
-  docker-machine create --driver generic --generic-ssh-key ~/.ssh/dockerengine_rsa --generic-ssh-user=dockerengine --generic-ip-address=$DE $DE
+  #docker-machine stop $DE
+  #docker-machine rm $DE
+  docker-machine create --driver generic --generic-ssh-key ~/.ssh/docker_rsa --generic-ssh-user=docker --generic-ip-address=$DE $DE
 done
 
 # show what is available
