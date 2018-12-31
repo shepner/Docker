b4ec679a0707276816ab172094726581ebb5136d
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2018-12-30 18:39:53 -0600

    Update and rename update-ubuntu.sh to dm_update-ubuntu.sh

--- Manager/HOME/bin/dm_update-ubuntu.sh
+++ Manager/HOME/bin/dm_update-ubuntu.sh
@@ -2,24 +2,22 @@
 #for some reason I had to use /bin/bash for /bin/sh stuff
 
 nodes[0]="de01a"
-nodes[1]="de02a"
-nodes[2]="de03a"
+nodes[1]="de01b"
+nodes[2]="de02a"
+nodes[3]="de02b"
+nodes[4]="de03a"
+nodes[5]="de03b"
 
 PATCHCMD="sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt -y autoremove"
 REBOOTCMD="sudo reboot"
 
 #patch the docker engines
-for DE in ${nodes[@]}; do
+for DE in ${nodes[@]} ; do
   docker-machine ssh $DE $PATCHCMD
 done
 
-#patch the local server
-$PATCHCMD
-
 #reboot the docker engines
-for DE in ${nodes[@]}; do
+for DE in ${nodes[@]} ; do
   docker-machine ssh $DE $REBOOTCMD
+  sleep 60
 done
-
-#reboot the local server
-$REBOOTCMD
