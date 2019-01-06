c0d88dacb7bb27aff3a2613bcc95d6537f3bca39
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-01-05 20:41:46 -0600

    Update plex.sh

--- Manager/docker/plex/plex.sh
+++ Manager/docker/plex/plex.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-
-# Look at this to use with plex plugins
-# https://github.com/linuxserver/docker-plex
-
-
 # https://forums.plex.tv/t/official-plex-media-server-docker-images-getting-started/172291
 # https://github.com/plexinc/pms-docker
 
-# The claim token can be found here:
-# https://www.plex.tv/claim/
-CLAIMTOKEN=""
+
+# The claim token can be found here: https://www.plex.tv/claim/
+CLAIMTOKEN=   # only needed the first time
 UID=1003
 GID=1100
 NETWORKS="10.0.0.0/8"
+
+
+# https://github.com/ZeroQI/Absolute-Series-Scanner
+PLEXDIR="/mnt/nas/docker/plex/config/Library/Application Support/Plex Media Server/Scanners"
+sudo mkdir -p "$PLEXDIR/Series"
+sudo wget -O "$PLEXDIR/Series/Absolute Series Scanner.py" https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
+sudo chown -R $UID:$GID "$PLEXDIR"
+sudo chmod 775 -R "$PLEXDIR"
+
 
 sudo docker service create \
   --name plex \
