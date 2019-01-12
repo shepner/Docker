dd94740589006ac7c2e0213bd5eff176d1b263f0
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-01-12 13:53:19 -0600

    Update bridgeroute.sh

--- Manager/docker/bridgeroute.sh
+++ Manager/docker/bridgeroute.sh
@@ -1,4 +1,5 @@
 
 
+https://developer.ibm.com/recipes/tutorials/bridge-the-docker-containers-to-external-network/
 https://jpetazzo.github.io/2013/10/16/configure-docker-bridge-network/
 https://docs.docker.com/v17.09/engine/swarm/networking/#customize-the-docker_gwbridge
