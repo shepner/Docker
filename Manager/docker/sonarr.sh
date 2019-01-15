5bcb1b3f7cb1e2d7670b2a7b9d674f7fc3934a7a
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-01-14 21:07:44 -0600

    Update sonarr.sh

--- Manager/docker/sonarr.sh
+++ Manager/docker/sonarr.sh
@@ -7,16 +7,20 @@ UID=1003
 GID=1100
 TIMEZONE="America/Chicago"
 
+mkdir -p /mnt/nas/docker/sonarr/config
+mkdir -p /mnt/nas/docker/sonarr/tv
+mkdir -p /mnt/nas/docker/sonarr/downloads
+
 sudo docker service create \
   --name sonarr \
   --publish published=8989,target=8989,protocol=tcp,mode=ingress \
-  --env PUID=<UID> \
-  --env PGID=<GID> \
-  --env TZ=$TIMEZONE \ 
+  --env PUID=$UID \
+  --env PGID=$GID \
+  --env TZ=$TIMEZONE \
   --mount type=bind,src=/etc/localtime,dst=/etc/localtime,readonly=1 \
-  --mount type=bind,src=,dst=/config \
-  --mount type=bind,src=,dst=/tv \
-  --mount type=bind,src=,dst=/downloads \
+  --mount type=bind,src=/mnt/nas/docker/sonarr/config,dst=/config \
+  --mount type=bind,src=/mnt/nas/docker/sonarr/tv,dst=/tv \
+  --mount type=bind,src=/mnt/nas/docker/sonarr/downloads,dst=/downloads \
   linuxserver/sonarr
 
 
