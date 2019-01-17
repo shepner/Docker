a2c23c7850e95017445fb223fbc83ee8e71e2406
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-01-16 20:51:46 -0600

    Create flexget.sh

---
+++ Manager/docker/download_automation/flexget.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# https://hub.docker.com/r/cpoppema/docker-flexget
+
+UID=1003
+GID=1100
+TIMEZONE="America/Chicago"
+BASEPATH=/mnt/nas/docker/flexget
+PASSWD=
+
+
+docker create \
+    --name=flexget \
+    -e PUID=$UID \
+    -e PGID=$GID \
+    -e TZ=$TIMEZONE \
+    -e WEB_PASSWD=$PASSWD \
+    -e TORRENT_PLUGIN=transmission \
+    -e FLEXGET_LOG_LEVEL=debug \
+    -p 5050:5050 \
+    -v $BASEPATH/config:/config \
+    -v $BASEPATH/downloads:/downloads \
+    cpoppema/docker-flexget
+    
+    
