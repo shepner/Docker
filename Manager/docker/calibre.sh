f29c3a6dc8ef9848c3bfbe1f4ec4dbe5b3efeb99
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 14:21:38 -0600

    Update calibre.sh

--- Manager/docker/calibre.sh
+++ Manager/docker/calibre.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
-# aptalca/docker-rdp-calibre
-# https://hub.docker.com/r/aptalca/docker-rdp-calibre
-#docker run \
-#  -d \
-#  --name="RDP-Calibre" \
-#  -e EDGE="0" \
-#  -e WIDTH="1280" \
-#  -e HEIGHT="720" \
-#  -v /path/to/config:/config:rw \
-#  -e TZ=America/New_York \
-#  -p XXXX:8080 \
-#  -p YYYY:8081 \
-#  aptalca/docker-rdp-calibre
 
-TIMEZONE="America/Chicago"
-BASEPATH=/mnt/nas/media
+CONFIG_PATH=/mnt/nas/docker/calibre
+LIBRARY_PATH=/mnt/nas/media/eBooks2
 
-mkdir -p $BASEPATH/eBooks2
+sudo mkdir -p $CONFIG_PATH
+sudo chown docker:docker $CONFIG_PATH
 
 sudo docker service create \
   --name Calibre \
   --publish published=6080,target=6080,protocol=tcp,mode=ingress \
+  --mount type=bind,src=$CONFIG_PATH,dst=/config \
+  --mount type=bind,src=$LIBRARY_PATH,dst=/Library \
   --replicas=1 \
   --constraint 'node.role != manager' \
   shepner/calibre
 
---mount type=bind,src=$BASEPATH/eBooks2,dst=/Library \
 
 
