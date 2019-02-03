1839d78e3c0a7c4244b2fe8c1837b9d36e2a452b
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 20:41:12 -0600

    Update calibre.sh

--- Manager/docker/calibre.sh
+++ Manager/docker/calibre.sh
@@ -9,12 +9,21 @@ sudo mkdir -p $CONFIG_PATH
 sudo chmod -R 775 $CONFIG_PATH
 sudo chown docker:docker $CONFIG_PATH
 
+#sudo mkdir -P $LIBRARY_PATH
+#sudo chmod -R 775 $LIBRARY_PATH
+#sudo chown docker:docker $LIBRARY_PATH
+
+sudo mkdir -P $LIBRARY_PATH-incoming
+sudo chmod -R 777 $LIBRARY_PATH-incoming
+sudo chown docker:docker $LIBRARY_PATH-incoming
+
 sudo docker service create \
   --name Calibre \
   --publish published=6080,target=6080,protocol=tcp,mode=ingress \
   --publish published=8080,target=8080,protocol=tcp,mode=ingress \
   --mount type=bind,src=$CONFIG_PATH,dst=/config \
   --mount type=bind,src=$LIBRARY_PATH,dst=/Library \
+  --mount type=bind,src=$LIBRARY_PATH-incoming,dst=/incoming \
   --replicas=1 \
   --constraint 'node.role != manager' \
   shepner/calibre
