5f24b53dcd8833ed660848dc95fa818508d85345
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 20:23:43 -0600

    Update calibre.sh

--- Manager/docker/calibre.sh
+++ Manager/docker/calibre.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# VNC Console:  6080
+# Calibre web interface:  8080
 
 CONFIG_PATH=/mnt/nas/docker/calibre
 LIBRARY_PATH=/mnt/nas/media/eBooks2
@@ -10,11 +12,11 @@ sudo chown docker:docker $CONFIG_PATH
 sudo docker service create \
   --name Calibre \
   --publish published=6080,target=6080,protocol=tcp,mode=ingress \
+  --publish published=8080,target=8080,protocol=tcp,mode=ingress \
   --mount type=bind,src=$CONFIG_PATH,dst=/config \
   --mount type=bind,src=$LIBRARY_PATH,dst=/Library \
   --replicas=1 \
   --constraint 'node.role != manager' \
   shepner/calibre
-
 
 
