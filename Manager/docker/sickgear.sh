04abd732e90fa02eb2e24fe222017a9039cd109c
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-03 14:46:27 -0500

    Create sickgear.sh

---
+++ Manager/docker/sickgear.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-sickgear
+
+NAME=sickgear
+UID=1003
+GID=1100
+TIMEZONE="America/Chicago"
+BASEPATH=/mnt/nas/docker/$NAME
+
+mkdir -p $BASEPATH/config
+mkdir -p $BASEPATH/tv
+mkdir -p $BASEPATH/downloads
+
+sudo docker service create \
+  --name=$NAME \
+  --constraint 'node.role != manager' \
+  --env PUID=$UID \
+  --env PGID=$GID \
+  --env TZ=$TIMEZONE \
+  --publish published=9091,target=8081,protocol=tcp,mode=ingress \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=$BASEPATH/tv,dst=/tv \
+  --mount type=bind,src=$BASEPATH/downloads,dst=/downloads \
+  linuxserver/sickgear
