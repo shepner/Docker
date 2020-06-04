9f4d365af3ba8d141c40d02b279bbff89b7410d3
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-03 19:57:28 -0500

    Update sonarr.sh

--- Manager/docker/downloads/sonarr.sh
+++ Manager/docker/downloads/sonarr.sh
@@ -6,22 +6,24 @@
 UID=1003
 GID=1100
 TIMEZONE="America/Chicago"
+NAME=sonarr
+BASEPATH=/mnt/nas/downloads/$NAME
 
-mkdir -p /mnt/nas/docker/sonarr/config
-mkdir -p /mnt/nas/docker/sonarr/tv
-mkdir -p /mnt/nas/docker/sonarr/downloads
+mkdir -p $BASEPATH/config
+mkdir -p $BASEPATH/tv
+mkdir -p $BASEPATH/downloads
 
 sudo docker service create \
-  --name sonarr \
+  --name $NAME \
   --constraint 'node.role != manager' \
-  --publish published=8989,target=8989,protocol=tcp,mode=ingress \
   --env PUID=$UID \
   --env PGID=$GID \
   --env TZ=$TIMEZONE \
+  --publish published=8989,target=8989,protocol=tcp,mode=ingress \
   --mount type=bind,src=/etc/localtime,dst=/etc/localtime,readonly=1 \
-  --mount type=bind,src=/mnt/nas/docker/sonarr/config,dst=/config \
-  --mount type=bind,src=/mnt/nas/docker/sonarr/tv,dst=/tv \
-  --mount type=bind,src=/mnt/nas/docker/sonarr/downloads,dst=/downloads \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=$BASEPATH/tv,dst=/tv \
+  --mount type=bind,src=$BASEPATH/downloads,dst=/downloads \
   linuxserver/sonarr
 
 
