e30e7d31d23e0b7308dbd0361e7fc03be4f17b4b
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-05 07:21:46 -0500

    Create jackett.sh

---
+++ Manager/docker/downloads/jackett.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-jackett
+
+UID=1003
+GID=1100
+TIMEZONE="America/Chicago"
+NAME=jackett
+BASEPATH=/mnt/nas/downloads/$NAME
+
+mkdir -p $BASEPATH/config
+mkdir -p $BASEPATH/downloads
+
+sudo docker service create \
+  --name $NAME \
+  --constraint 'node.role != manager' \
+  --env PUID=$UID \
+  --env PGID=$GID \
+  --env TZ=$TIMEZONE \
+  --env AUTO_UPDATE=true \
+  --publish published=9117,target=9117,protocol=tcp,mode=ingress \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=$BASEPATH/downloads,dst=/downloads \
+  linuxserver/jackett
+  
