93d951e77459d59e215cf79dbdec17cf88c882ca
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-04 19:23:37 -0500

    Update sickgear.sh

--- Manager/docker/downloads/sickgear.sh
+++ Manager/docker/downloads/sickgear.sh
@@ -5,11 +5,11 @@ NAME=sickgear
 UID=1003
 GID=1100
 TIMEZONE="America/Chicago"
-BASEPATH=/mnt/nas/docker/$NAME
+BASEPATH=/mnt/nas/downloads/$NAME
 
 mkdir -p $BASEPATH/config
 mkdir -p $BASEPATH/tv
-mkdir -p $BASEPATH/downloads
+#mkdir -p $BASEPATH/downloads
 
 sudo docker service create \
   --name=$NAME \
@@ -20,5 +20,5 @@ sudo docker service create \
   --publish published=9091,target=8081,protocol=tcp,mode=ingress \
   --mount type=bind,src=$BASEPATH/config,dst=/config \
   --mount type=bind,src=$BASEPATH/tv,dst=/tv \
-  --mount type=bind,src=$BASEPATH/downloads,dst=/downloads \
+  --mount type=bind,src=/mnt/nas/downloads/transmission/downloads,dst=/downloads \
   linuxserver/sickgear
