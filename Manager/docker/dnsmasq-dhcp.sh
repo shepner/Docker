0675f517c7dbf7c07c8b3a576303449098d43901
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-10 08:16:04 -0500

    Create dnsmasq-dhcp.sh

---
+++ Manager/docker/dnsmasq-dhcp.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# https://github.com/shepner/Docker-DNSmasq
+
+# WARNING: This is NOT a Docker Swarm service and runs with privledged rights
+
+
+NAME=DNSmasq-dhcp
+BASEPATH=/mnt/nas/docker/$NAME
+
+#mkdir -p $BASEPATH/config
+#mkdir -p $BASEPATH/webproc_dhcp
+
+#wget -O $BASEPATH/webproc_dhcp/program.toml https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/program.toml
+#wget -O $BASEPATH/config/dnsmasq_dhcp.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
+#wget -O $BASEPATH/config/dnsmasq.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
+#wget -O $BASEPATH/resolv.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/resolv.conf
+
+sudo docker create \
+  --name=$NAME \
+  --restart unless-stopped \
+  --privileged \
+  --env WEBPROC_CONF=/mnt/webproc_dhcp/program.toml \
+  --mount type=bind,src=/mnt/nas/docker/dnsmasq,dst=/mnt \
+  --network host \
+  shepner/dnsmasq:latest
+  
