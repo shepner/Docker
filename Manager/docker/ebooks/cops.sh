e602ff810b3c55ed3bb3208db04f38ac81c1e782
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-10 20:52:28 -0500

    Create cops.sh

---
+++ Manager/docker/ebooks/cops.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-cops
+# http://blog.slucas.fr/en/oss/calibre-opds-php-server
+
+NAME=cops
+UID=1003
+GID=1100
+TIMEZONE="America/Chicago"
+BASEPATH=/mnt/nas/docker/$NAME
+
+mkdir -p $BASEPATH/config
+#mkdir -p $BASEPATH/books
+
+sudo docker service create \
+  --name=$NAME \
+  --constraint 'node.role != manager' \
+  --env PUID=$UID \
+  --env PGID=$GID \
+  --env TZ=$TIMEZONE \
+  --publish published=8000,target=80,protocol=tcp,mode=ingress \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=/mnt/nas/media/eBooks,dst=/books \
+  linuxserver/cops
