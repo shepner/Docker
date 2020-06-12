4ecd6c8e094c1f70f587bf78e059ad3fa6172e26
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-12 14:30:08 -0500

    Update airflow.sh

--- Manager/docker/airflow/airflow.sh
+++ Manager/docker/airflow/airflow.sh
@@ -2,6 +2,8 @@
 # https://bitnami.com/stack/apache-airflow
 # https://github.com/bitnami/bitnami-docker-airflow
 
+# https://github.com/ContainX/docker-volume-netshare
+
 
 curl -LO https://raw.githubusercontent.com/bitnami/bitnami-docker-airflow/master/docker-compose.yml
 sudo docker-compose up
