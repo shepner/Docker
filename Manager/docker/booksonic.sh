ff9a7f8040d171b8b945596ddd35e439208c47a7
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-06-15 19:59:44 -0500

    Create booksonic.sh

---
+++ Manager/docker/booksonic.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-sonarr
+
+UID=1003
+GID=1000
+TIMEZONE="America/Chicago"
+NAME=booksonic
+BASEPATH=/mnt/nas/docker/$NAME
+
+mkdir -p $BASEPATH/config
+#mkdir -p $BASEPATH/tv
+#mkdir -p $BASEPATH/downloads
+
+sudo docker service create \
+  --name $NAME \
+  --constraint 'node.role != manager' \
+  --env PUID=$UID \
+  --env PGID=$GID \
+  --env TZ=$TIMEZONE \
+  --publish published=4040,target=4040,protocol=tcp,mode=ingress \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=/mnt/nas/media/Audio\ Books,dst=/audiobooks \
+  linuxserver/booksonic
+  
+
+#  -e CONTEXT_PATH=url-base \
+#  -v </path/to/podcasts>:/podcasts \
+#  -v </path/to/othermedia>:/othermedia \
+  
