55beefd122dadb787a92e0e163c678f7fb92d806
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-07-02 15:15:21 -0500

    Update jackett.sh

--- Manager/docker/downloads/jackett.sh
+++ Manager/docker/downloads/jackett.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # https://docs.linuxserver.io/images/docker-jackett
 
-UID=1003
-GID=1000
+#CUID=1003
+#CGID=1000
+CUID=0
+CGID=0
 TIMEZONE="America/Chicago"
 NAME=jackett
 BASEPATH=/mnt/nas/downloads/$NAME
@@ -14,8 +16,8 @@ sudo docker service create \
   --name $NAME \
   --hostname $NAME \
   --constraint 'node.role != manager' \
-  --env PUID=$UID \
-  --env PGID=$GID \
+  --env PUID=$CUID \
+  --env PGID=$CGID \
   --env TZ=$TIMEZONE \
   --env AUTO_UPDATE=true \
   --publish published=9117,target=9117,protocol=tcp,mode=ingress \
