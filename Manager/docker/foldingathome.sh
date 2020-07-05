ec0dfee9e9ab534cbde4b2a58d20cb2f3493ba93
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-07-04 20:43:23 -0500

    Update foldingathome.sh

--- Manager/docker/foldingathome.sh
+++ Manager/docker/foldingathome.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # https://docs.linuxserver.io/images/docker-foldingathome
 
-UID=1003
-GID=1100
+CUID=1003
+CGID=1000
 TIMEZONE="America/Chicago"
 NAME=foldingathome
 BASEPATH=/mnt/nas/docker/$NAME
@@ -12,8 +12,8 @@ mkdir -p $BASEPATH/config
 sudo docker service create \
   --name $NAME \
   --constraint 'node.role != manager' \
-  --env PUID=$UID \
-  --env PGID=$GID \
+  --env PUID=$CUID \
+  --env PGID=$CGID \
   --env TZ=$TIMEZONE \
   --publish published=7396,target=7396,protocol=tcp,mode=ingress \
   --mount type=bind,src=$BASEPATH/config,dst=/config \
