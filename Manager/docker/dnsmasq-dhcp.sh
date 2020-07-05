8c210abb59dc7764f6fff9b0c8981ac39c9811d9
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-07-04 23:33:32 -0500

    Update dnsmasq-dhcp.sh

--- Manager/docker/dnsmasq-dhcp.sh
+++ Manager/docker/dnsmasq-dhcp.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 # https://github.com/shepner/Docker-DNSmasq
 
+# Combined DNS and DHCP server
 # WARNING: This is NOT a Docker Swarm service and runs with privledged rights
 
-
-#NAME=DNSmasq-dhcp
-#BASEPATH=/mnt/nas/docker/dnsmasq
+NAME=dnsmasq
+BASEPATH=/mnt/nas/docker/$NAME
 
 #mkdir -p $BASEPATH/config
-#mkdir -p $BASEPATH/webproc_dhcp
-
-#wget -O $BASEPATH/webproc_dhcp/program.toml https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/program.toml
 #wget -O $BASEPATH/config/dnsmasq_dhcp.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
 #wget -O $BASEPATH/config/dnsmasq.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/dnsmasq.conf
 #wget -O $BASEPATH/resolv.conf https://raw.githubusercontent.com/shepner/Docker-DNSmasq/master/resolv.conf
 
 sudo docker create \
-  --name=DNSmasq-dhcp \
+  --name=$NAME \
   --restart unless-stopped \
   --privileged \
-  --env WEBPROC_CONF=/mnt/webproc_dhcp/program.toml \
+  --env DNSMASQ_CONF=/mnt/config/dnsmasq_combined.conf \
   --mount type=bind,src=/mnt/nas/docker/dnsmasq,dst=/mnt \
   --network host \
   shepner/dnsmasq:latest
+
+sudo docker start dnsmasq
