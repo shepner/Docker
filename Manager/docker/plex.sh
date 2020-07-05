bff44eb2481815c39ea7c1ac31f3fbba52cfffdf
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-07-05 17:16:24 -0500

    Update plex.sh

--- Manager/docker/plex.sh
+++ Manager/docker/plex.sh
@@ -10,7 +10,7 @@
 
 
 # With this script, the directories:
-# '/var/lib/plexmediaserver' (Ubuntu) and '/mnt/nas/docker/plex/config' (Docker container)
+# '/var/lib/plexmediaserver' (Ubuntu) and '/mnt/nas/docker1/plex/config' (Docker container)
 # are the same thing.  In the event you are migrating from a Linux (Ubuntu) server to a Docker
 # container, it is possible to move the original Library folder to a new (shared) location
 # and point the container at it.  You will also want to do the same with your media folder.
@@ -21,14 +21,53 @@
 # a very transparant method to migrate from the old server to the new container and back
 # be sure the file permissions are correct on all folders
 
+# From the linux Plex server:
+sudo cp -R /var/lib/plexmediaserver /mnt/nas/docker1/plex
+sudo mv /mnt/nas/docker1/plex/plexmediaserver /mnt/nas/docker1/plex/config
+
+
 # The claim token is only needed with a new setup
 # https://www.plex.tv/claim/
 CLAIMTOKEN=
 
-NETWORKS="10.0.0.0/8"
-UID=1003
-GID=1100
+CUID=1003
+CGID=1000
 TIMEZONE="America/Chicago"
+NETWORKS="10.0.0.0/8"
+
+BASEDIR=/mnt/nas/docker1/plex
+sudo mkdir -p $BASEDIR/config
+sudo mkdir -p $BASEDIR/transcode
+
+# install/update the Absolute Series Scanner plugin
+# https://github.com/ZeroQI/Absolute-Series-Scanner
+PLEXDIR="$BASEDIR/config/Library/Application Support/Plex Media Server/Scanners"
+sudo mkdir -p "$PLEXDIR/Series"
+sudo wget -O "$PLEXDIR/Series/Absolute Series Scanner.py" https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
+sudo chown -R $CUID:$CGID "$PLEXDIR"
+sudo chmod 775 -R "$PLEXDIR"
+
+# https://github.com/plexinc/pms-docker
+# https://forums.plex.tv/t/official-plex-media-server-docker-images-getting-started/172291
+sudo docker create \
+  --restart unless-stopped \
+  --name plex \
+  --network=host \
+  --env TZ=$TIMEZONE \
+  --env PLEX_CLAIM="$CLAIMTOKEN" \
+  --env PLEX_UID=$CUID \
+  --env PLEX_GID=$CGID \
+  --env ALLOWED_NETWORKS=$NETWORKS \
+  --mount type=bind,src=/mnt/nas/docker1/plex/config,dst=/config \
+  --mount type=bind,src=/mnt/nas/docker1/plex/transcode,dst=/transcode \
+  --mount type=bind,src=/mnt/nas/media,dst=/data \
+  plexinc/pms-docker \
+
+# start the Plex service (slow to start!)
+sudo docker start plex
+
+########################################
+#old stuff that prolly could be deleted
 
 NODE=de01
 NETNME=bridge_plex
@@ -52,10 +91,10 @@ ssh $NODE "sudo iptables -P FORWARD ACCEPT"
 
 # install/update the Absolute Series Scanner plugin
 # https://github.com/ZeroQI/Absolute-Series-Scanner
-PLEXDIR="/mnt/nas/docker/plex/config/Library/Application Support/Plex Media Server/Scanners"
+PLEXDIR="/mnt/nas/docker1/plex/config/Library/Application Support/Plex Media Server/Scanners"
 sudo mkdir -p "$PLEXDIR/Series"
 sudo wget -O "$PLEXDIR/Series/Absolute Series Scanner.py" https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
-sudo chown -R $UID:$GID "$PLEXDIR"
+sudo chown -R $CUID:$CGID "$PLEXDIR"
 sudo chmod 775 -R "$PLEXDIR"
 
 
@@ -70,8 +109,8 @@ ssh $NODE " \
   --ip $IPADDR \
   --env TZ=$TIMEZONE \
   --env PLEX_CLAIM="$CLAIMTOKEN" \
-  --env PLEX_UID=$UID \
-  --env PLEX_GID=$GID \
+  --env PLEX_UID=$CUID \
+  --env PLEX_GID=$CGID \
   --env ALLOWED_NETWORKS=$NETWORKS \
   --volume type=bind,src=/mnt/nas/docker/plex/config,dst=/config \
   --volume type=bind,src=/mnt/nas/docker/plex/transcode,dst=/transcode \
