aaa4e29b5e84f19b790b3d3eb4caf6b4b55cc526
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-07-11 18:29:18 -0500

    Update syncthing.sh

--- Manager/docker/syncthing.sh
+++ Manager/docker/syncthing.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-# https://github.com/linuxserver/docker-syncthing
+# https://docs.linuxserver.io/images/docker-syncthing
 
-CONFIG_PATH=/mnt/nas/docker/syncthing
-DATA_PATH=/mnt/nas/media/syncthing/
+NAME=syncthing
+CUID=1003
+CGID=1000
+TIMEZONE="America/Chicago"
+BASEPATH=/mnt/nas/docker1/syncthing
 
-sudo docker create \
-  --name syncthing \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ="America/Chicago" \
+sudo mkdir -p $BASEPATH/config
+sudo mkdir -p $BASEPATH/data1
+sudo mkdir -p $BASEPATH/data2
+
+sudo docker service create \
+  --name $NAME \
+  --env PUID=$CUID \
+  --env PGID=$CGID \
+  --env TZ=$TIMEZONE \
+  --constraint 'node.role != manager' \
   --publish published=8384,target=8384,protocol=tcp,mode=ingress \
   --publish published=22000,target=22000,protocol=tcp,mode=ingress \
   --publish published=21027,target=21027,protocol=udp,mode=ingress \
-  --mount type=bind,src=$CONFIG_PATH/config,dst=/config \
-  --mount type=bind,src=$DATA_PATH/data1,dst=/data1 \
-  --mount type=bind,src=$DATA_PATH/data2,dst=/data2 \
-  --restart unless-stopped \
+  --mount type=bind,src=$BASEPATH/config,dst=/config \
+  --mount type=bind,src=$BASEPATH/data1,dst=/data1 \
+  --mount type=bind,src=$BASEPATH/data2,dst=/data2 \
   linuxserver/syncthing
